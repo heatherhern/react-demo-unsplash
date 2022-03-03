0459c2000df0402d39b540d99cfce4783dae00d4
Heather Hern <heathernewlin1@gmail.com>
2022-03-03 18:02:10 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const url = "https://api.unsplash.com/photos/?client_id=EFPVpScTkPLXh1g-f7SXLo62iM5XrmDH6jCGfeqHdDs"
+  const [unsplashArray, setUnsplashArray] = useState([]);
+  
+  const sendGetRequest = async () => {
+    try {
+      const response = await axios.get(url);
+      setUnsplashArray(response.data);
+      console.log(response);
+      console.log(unsplashArray);
+    } catch (err) {
+      console.error(err);
+    }
+    sendGetRequest();
+    return ("");
+    // <div>
+    // <h1>my react app</h1>
+    //   const photosArray = {unsplashArray.map((item) => (
+    //   <img src={photosArray.data.url}
+  };
 }
+
 
 export default App;
