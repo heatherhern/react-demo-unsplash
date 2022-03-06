277074e7e0aa221c65344562d3abf24116a6f61e
Heather Hern <heathernewlin1@gmail.com>
2022-03-06 16:18:28 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-useEffect(() => {
-  const sendGetRequest = async () => {
-    try {
-      const res = await axios.get(url);
-      setUnsplashArray(res.data);
-      console.log(res.data);
-      // console.log(unsplashArray);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-});
+
 
 function App() {
   const url = "https://api.unsplash.com/photos/?client_id=EFPVpScTkPLXh1g-f7SXLo62iM5XrmDH6jCGfeqHdDs"
   const [unsplashArray, setUnsplashArray] = useState([]);
-  
-  
-    
+
+  useEffect(() => {
+    axios.get(url);
+    setUnsplashArray(res.data);
+    console.log(res.data);
+  }, []);
+
     sendGetRequest();
     useEffect(()
 
@@ -32,7 +25,7 @@ function App() {
         //   <img src={photosArray.data.url}
     // };
 };
-// }
+
 
 
 
